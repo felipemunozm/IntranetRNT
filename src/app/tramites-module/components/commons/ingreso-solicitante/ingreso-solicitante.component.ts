ab78df7a8f9c1d248905d34fd405ce964706a893
Felipe Munoz <felipe.munozm@gmail.com>
2020-06-30 04:16:37 -0400

    ingreso solicitante try 1

--- src/app/tramites-module/components/commons/ingreso-solicitante/ingreso-solicitante.component.ts
+++ src/app/tramites-module/components/commons/ingreso-solicitante/ingreso-solicitante.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
 import { Solicitante } from '../../../models/solicitante.model'
 
 @Component({
@@ -6,13 +6,28 @@ import { Solicitante } from '../../../models/solicitante.model'
   templateUrl: './ingreso-solicitante.component.html',
   styleUrls: ['./ingreso-solicitante.component.css']
 })
-export class IngresoSolicitanteComponent implements OnInit {
+export class IngresoSolicitanteComponent implements OnInit, DoCheck {
 
+  public isEmpresa
+  public isPersona
+  public isMandatario
   public solicitante: Solicitante
   constructor() {
+    this.solicitante = new Solicitante('', '', '', 0, '', '')
   }
 
   ngOnInit(): void {
   }
 
+  ngDoCheck() {
+    console.log("isPersona: " + this.isPersona)
+    console.log("isMandatario: " + this.isMandatario)
+  }
+  isMandatarioChange(checked) {
+    console.log("Llego: " + checked)
+    this.isMandatario = checked
+  }
+  checkOnChange() {
+    console.log("isMandatario: " + this.isMandatario)
+  }
 }
--- src/app/tramites-module/models/solicitante.model.ts
+++ src/app/tramites-module/models/solicitante.model.ts
@@ -1,8 +1,13 @@
+import { StringMap } from '@angular/compiler/src/compiler_facade_interface';
+
 export class Solicitante {
     constructor(
         public name: string,
         public run: string,
-        public email: string
+        public email: string,
+        public region: number,
+        public rutEmpresa: String,
+        public runMandatario: string,
     ) {
 
 
